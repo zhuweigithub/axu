9d8b340ff75a2886eed3376f821996772ad8524d
zw <452436132@qq.com>
2016-07-14 14:14:12 +0800

    11

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -10,4 +10,9 @@ return array(
         "appSecret"=>'7857a1c0ae6a07ce8a256ca8e155a91e',
         "returnUrl"=>"weixin.aweb.ybzf.com"
     ),
+    /* URL配置 */
+    'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小写 true则表示不区分大小写
+    'URL_MODEL'            => 3, //URL模式
+    'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
+    'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
 );
