b6e405dcba4061b61b0eae1793271ccfcc167a38
zhuwei <452436132@qq.com>
2016-07-24 19:38:30 +0800

    1

---
+++ Application/Home/Controller/LoginController.class.php
@@ -0,0 +1,22 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class LoginController extends BaseController {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function login(){
+        $data['username']='';//json 格式  参数需要组装   username     password  这里参数需要些验证什么的，
+        $data['password']='';
+        $verify = D("Login");
+        $result = $verify -> login( $data );//登录的接口
+        fb($result);
+        $this->display();
+    }
+    public function loginVerify(){
+
+
+        $this->display();
+    }
+}
