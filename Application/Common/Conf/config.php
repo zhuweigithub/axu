ef6e0d6e3e8ee6780efba3caa72a820d49d6f6bf
manage <1191502428@qq.com>
2016-08-10 15:31:00 +0800

    111

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -10,27 +10,17 @@ return array(
     'URL_MODEL'            => 3, //URL模式
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
     'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割符
-<<<<<<< HEAD
     /* 模板相关配置 */
     'TMPL_PARSE_STRING' => array(
         '__IMG__'    => __ROOT__ . '/Public/images',
         '__CSS__'    => __ROOT__ . '/Public/css',
         '__JS__'     => __ROOT__ . '/Public/js',
     ),
-=======
->>>>>>> 1fb7570049b95464eece187277ad75a74607bd50
 
     /* 数据库配置 */
     'DB_TYPE'   => 'mysqli', // 数据库类型
     'DB_HOST'   => '120.26.231.149', // 服务器地址
-<<<<<<< HEAD
     'DB_NAME'   => 'ax', // 数据库名
-    'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'zw2732.com',  // 密码fe9a28f01e
-    'DB_PORT'   => '3306', // 端口
-    'DB_PREFIX' => 'oa_', // 数据库表前缀
-=======
-    'DB_NAME'   => 'aixiu', // 数据库名
     'DB_USER'   => 'root', // 用户名
     'DB_PWD'    => 'fe9a28f01e',  // 密码fe9a28f01e
     'DB_PORT'   => '3306', // 端口
@@ -38,5 +28,4 @@ return array(
 
     /*加密字符串*/
     "PASSWORD_SUFFIX" => 'zw_info'
->>>>>>> 1fb7570049b95464eece187277ad75a74607bd50
 );
