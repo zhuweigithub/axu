79056cfec9785c6017967a961aa4b0900384583e
zw <452436132@qq.com>
2016-08-12 11:25:04 +0800

    1

--- Application/Common/Api/WxApi.class.php
+++ Application/Common/Api/WxApi.class.php
@@ -12,7 +12,7 @@ class WxApi{
     public function getList( $code ){
         $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$this->_appId&secret=$this->_appSecret";
         $token = $this->getJson($url);
-
+        session("zw1",$token);exit;
         //第二步:取得openid
         $oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$this->_appId&secret=$this->_appSecret&code=$code&grant_type=authorization_code";
         $oauth2 = $this->getJson($oauth2Url);
@@ -20,7 +20,7 @@ class WxApi{
         //第三步:根据全局access_token和openid查询用户信息
         $access_token = $token["access_token"];
         $openid = $oauth2['openid'];
-        session("zw1",$oauth2);exit;
+
         $get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
         $userInfo = $this->getJson($get_user_info_url);
         session("zw",$userInfo);
