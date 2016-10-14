9a2848df212dfcb8c3c1a18f9df274b4e68b2e50
zw <452436132@qq.com>
2016-10-14 14:19:01 +0800

    1

--- Public/js/wx.method.js
+++ Public/js/wx.method.js
@@ -43,21 +43,17 @@ function sss(){
 			'chooseCard',
 			'openCard'
 		];
-		data.debug = true;
+		data.debug = false;
 		wx.config(data);
 	});
 }
 $(".uploadImg").on("click",function(){
-	alert(111);
 	wx.ready(function() {
-		alert(33);
 		wx.chooseImage({
 			count: 1, // 默认9
 			sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
 			sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
 			success: function (res) {
-				console.log(res);
-				alert(res);
 				var localId = res.localIds[0]; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
 				if (localId.indexOf("wxlocalresource") != -1) {
 					localId = localId.replace("wxlocalresource", "wxLocalResource");
