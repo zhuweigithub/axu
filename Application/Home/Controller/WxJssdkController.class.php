634278c39340fe0a26e86e976b75cec338e3ee1d
zw <452436132@qq.com>
2016-10-14 11:12:21 +0800

    11

--- Application/Home/Controller/WxJssdkController.class.php
+++ Application/Home/Controller/WxJssdkController.class.php
@@ -84,6 +84,7 @@ class WxJssdkController extends FatherController{
         curl_setopt($curl, CURLOPT_TIMEOUT, 500);
         curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($curl, CURLOPT_POST, 1 );
         curl_setopt($curl, CURLOPT_URL, $url);
         $res = curl_exec($curl);
         curl_close($curl);
